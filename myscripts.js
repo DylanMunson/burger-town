3a866991b015a5f6bac12f9775522b637a92c599
Dylan Munson <dylanmunson99@gmail.com>
2020-09-26 04:38:13 -0700

    added js function

--- myscripts.js
+++ myscripts.js
@@ -2,16 +2,27 @@ var today = new Date ();
 var hourNow = today.getHours();
 var greeting;
 
-if (hourNow > 17) {
+if (hourNow > 18) {
     greeting = 'Dinner Time!';
-} else if (hourNow > 11) {
+} else if (hourNow > 12) {
     greeting = 'Lunch Time!';
-} else if (hourNow > 5) {
+} else if (hourNow > 0) {
     greeting = 'Breakfast Time!';
-} else if (hourNow > 22) {
-    greeting = 'Late Night Snack!';
 } else {
     greeting = 'Welcome!';
 }
 
 document.write('<h3>' + greeting + '</h3>');
+
+var name = prompt('What is your name?');
+alert('Hey, ' + name + '. Welcome to my outstanding, wonderful, and beautiful website!' );
+
+var button = document.createElement("button");
+button.innerHTML = "Free Food";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  alert("OUT OF STOCK");
+});
